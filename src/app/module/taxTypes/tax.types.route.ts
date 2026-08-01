c44ce415e7a711a6eacbf3025de45e2c18b234c9
farukulWD <farukgb1999@gmail.com>
2026-08-01 14:47:57 +0600

    feat: enhance tax type creation and update with file upload support

--- src/app/module/taxTypes/tax.types.route.ts
+++ src/app/module/taxTypes/tax.types.route.ts
@@ -1,15 +1,25 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { TaxTypeController } from './tax.types.controller';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../users/user.constant';
 import validateRequest from '../../middlewares/validateRequest';
+import { upload } from '../../utils/sendImageToCloudinary';
 import { TaxTypeValidation } from './tax.types.validation';
 
 const route = Router();
 
+const parseFormData = (req: Request, res: Response, next: NextFunction) => {
+  if (req.body.data) {
+    req.body = JSON.parse(req.body.data);
+  }
+  next();
+};
+
 route.post(
   '/create-tax-type',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  upload.single('icon'),
+  parseFormData,
   validateRequest(TaxTypeValidation.createTaxTypeValidationSchema),
   TaxTypeController.createTaxType,
 );
@@ -21,6 +31,8 @@ route.get(
 route.patch(
   '/update-tax-type/:id',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  upload.single('icon'),
+  parseFormData,
   validateRequest(TaxTypeValidation.updateTaxTypeValidationSchema),
   TaxTypeController.updateTaxType,
 );
